d9729665aeecda856c5a0c4794635cd3070bb43a
Gasper <gasper@gmx.com>
2019-08-13 18:28:10 +0200

    Created draft timeline component and added it to main page

---
+++ timeline/src/components/Timeline.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div id='timeline' ref='timeline' />
+  </div>
+</template>
+
+<script>
+import {DataSet, Timeline} from 'vis-timeline/standalone/umd/vis-timeline-graph2d.min.js';
+
+export default {
+  name: 'Timeline',
+  data() {
+    return {
+      timelineInstance: null,
+    };
+  },
+  mounted() {
+    let dataset = new DataSet([]);
+    let options = {};
+
+    this.timelineInstance = new Timeline(this.$refs.timeline, dataset, options);
+  },
+}
+</script>
