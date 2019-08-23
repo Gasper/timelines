1755cab08e6c827418793bd85f6719f580deca75
Gasper <gasper@gmx.com>
2019-08-23 20:06:57 +0200

    Added fetching categories and groups to graphQL API

--- timeline_api/db/mongo_db.py
+++ timeline_api/db/mongo_db.py
@@ -4,7 +4,15 @@ import pymongo
 class MongoDatabase:
     def __init__(self):
         client = pymongo.MongoClient('localhost', 27017)
-        self.collection = client.events.events
+        self.categories_collection = client.timeline.categories
+        self.groups_collection = client.timeline.groups
+        self.events_collection = client.timeline.events
+    
+    def get_categories(self):
+        return self.categories_collection.find()
+    
+    def get_groups(self):
+        return self.groups_collection.find()
 
     def get_events(self, start, end, group_id):
         query = {
@@ -12,7 +20,7 @@ class MongoDatabase:
             'end': {'$gt': start},
             'group_id': {'$eq': group_id},
         }
-        return self.collection.find(query)
+        return self.events_collection.find(query)
 
     def get_event(self, event_id):
-        return self.collection.find_one({'id': event_id})
+        return self.events_collection.find_one({'id': event_id})
--- timeline_api/graphql_api.py
+++ timeline_api/graphql_api.py
@@ -62,12 +62,26 @@ class Event(ObjectType):
         return event.get('group_id')
 
 class Query(ObjectType):
+    categories = Field(List(of_type=Category))
+
+    groups = Field(List(of_type=Group))
+
     event = Field(Event, event_id=ID(required=True))
 
     events = Field(List(of_type=Event),
                    start_time=String(required=True),
                    end_time=String(required=True),
                    group=String(required=True))
+
+    @staticmethod
+    def resolve_categories(root, info):
+        database = info.context.get('database')
+        return database.get_categories()
+
+    @staticmethod
+    def resolve_groups(root, info):
+        database = info.context.get('database')
+        return database.get_groups()
 
     @staticmethod
     def resolve_event(root, info, event_id):
