6511240ad0aa071dd1eb63c0204633dffd6de2ef
Gasper <gasper@gmx.com>
2019-08-23 21:01:31 +0200

    Added timeline API interface for fetching events, groups and categories

---
+++ timeline/src/components/TimelineApi.js
@@ -0,0 +1,50 @@
+/* eslint indent: "off" */
+
+class TimelineApi {
+  constructor(graphqlEndpoint) {
+    this.graphqlEndpoint = graphqlEndpoint;
+  }
+
+  async fetchGraphql(query) {
+    const eventFetch = await fetch(new Request(this.graphqlEndpoint), {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'text/plain',
+        Accept: 'application/json',
+      },
+      body: query,
+    });
+
+    if (!eventFetch.ok) {
+      throw new Error(`HTTP error! status: ${eventFetch.status}`);
+    }
+    
+    return await eventFetch.json();
+  }
+
+  async getGroups() {
+    const queryResult = await this.fetchGraphql(`
+      query {
+        categories {id, name}
+        groups {id, name, categoryId}
+      }`);
+
+    return queryResult;
+  }
+
+  async getEvents(startDateTime, endDateTime, groupId) {
+    const queryResult = await this.fetchGraphql(`
+      query {
+        events(startTime: "${startDateTime}", endTime: "${endDateTime}",
+        groupId: "${groupId}") {
+          id, title, description,
+          start, end,
+          groupId
+      }
+    }`);
+
+    return queryResult.events;
+  }
+}
+
+export default TimelineApi;
