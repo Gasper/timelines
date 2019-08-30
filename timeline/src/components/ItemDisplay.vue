1c15343c45505b3a25aaa444c718b713a30b3676
Gasper <gasper@gmx.com>
2019-08-30 17:03:27 +0200

    Fixed overflow styling on the timeline and added a component for displaying event description

---
+++ timeline/src/components/ItemDisplay.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="item-display" class="card">
+    <h5 class="card-header">{{ title }}</h5>
+    <p class="card-text">{{ content }}</p>
+  </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default {
+  name: 'ItemDisplay',
+  props: {
+    title: String,
+    content: String,
+  },
+};
+</script>
+
+<style>
+#item-display {
+  min-height: 200px;
+  margin: 7px;
+}
+</style>
--- timeline/src/components/Timeline.vue
+++ timeline/src/components/Timeline.vue
@@ -80,6 +80,6 @@ export default {
 #timeline {
   box-sizing: border-box;
   width: 100%;
-  height: 300px;
+  height: 100%;
 }
 </style>
