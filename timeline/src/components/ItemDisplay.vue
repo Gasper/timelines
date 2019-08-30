76280006528440bf3d5175be743a644218679e42
Gasper <gasper@gmx.com>
2019-08-30 17:29:32 +0200

    Markdown parsing and HTML sanitization of the item description

--- timeline/src/components/ItemDisplay.vue
+++ timeline/src/components/ItemDisplay.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="item-display" class="card">
     <h5 class="card-header">{{ title }}</h5>
-    <p class="card-text">{{ content }}</p>
+    <div class="card-text" v-html="markdownContent"></div>
   </div>
 </template>
 
 <script>
 import 'bootstrap/dist/css/bootstrap.css';
+import DOMPurify from 'dompurify/dist/purify.min.js';
+import marked from 'marked/marked.min.js';
 
 export default {
   name: 'ItemDisplay',
   props: {
     title: String,
     content: String,
+  },
+  computed: {
+    markdownContent() {
+      return DOMPurify.sanitize(marked(this.content));
+    },
   },
 };
 </script>
